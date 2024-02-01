5da2b5ecdd836f06064dd7c5657be481f8a88c8e
윤석규 <sg.yun@joongonara.co.kr>
2024-02-01 22:29:45 +0900

    [feat] : 카테고리 영역 지정
    - 카테고리 리스트를 불러오는 동안 loader 컴포넌트를 보여주도록 Suspense 지정
    - Flex 컴포넌트로 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import Layout from './components/common/layout';
+import { Suspense } from 'react';
+import CategoryList from '@components/category';
+import Layout from '@components/common/layout';
+import Flex from '@components/common/flex';
+import CategoryLoader from './components/loader/category/loader';
 
 function App() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Flex>
+        <Flex.Row>
+          <Suspense fallback={<CategoryLoader />}>
+            <CategoryList />
+          </Suspense>
+        </Flex.Row>
+      </Flex>
+    </Layout>
+  );
 }
 
 export default App;
