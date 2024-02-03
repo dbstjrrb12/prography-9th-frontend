84b88e79bf83951d53804d23f1f21f168a0d612d
윤석규 <sg.yun@joongonara.co.kr>
2024-02-03 21:01:47 +0900

    [fix] : 이미지 위치 변동 및 import 문 수정

--- src/components/common/layout.tsx
+++ src/components/common/layout.tsx
@@ -4,7 +4,7 @@ import Image from '@components/common/image';
 
 import cn from '@utils/cn';
 
-import logoImage from '@public/logo.png';
+import logoImage from '@assets/image/logo.png?url';
 
 interface Props extends PropsWithChildren {
   className?: string;
@@ -14,13 +14,7 @@ const Layout = ({ className, children }: Props) => {
   return (
     <div className={cn('w-[100vw] px-20', className)}>
       <header className="w-full pt-12 pb-8 fixed bg-white z-10">
-        <Image
-          src={logoImage}
-          alt="프로그라피 로고"
-          width={134}
-          height={48}
-          lazy={false}
-        />
+        <Image src={logoImage} alt="프로그라피 로고" width={134} height={48} lazy={false} />
       </header>
       <main className="pt-top-header">{children}</main>
     </div>
