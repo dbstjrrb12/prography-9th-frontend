aad9cde69d7b1be65881b1f8cc848b14e419adde
윤석규 <sg.yun@joongonara.co.kr>
2024-02-04 08:58:04 +0900

    [feat] : preview port 번호 변경

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  preview: {
+    port: 3000,
+    open: true,
+  },
   server: {
     port: 3000,
     open: true,
